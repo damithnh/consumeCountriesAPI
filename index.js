a55d240ae63f043ee9a87dc619db851066a7e843
unknown <brabbit640@gmail.com>
2021-06-08 00:46:24 +0530

    init

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use('/api/countries', genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
